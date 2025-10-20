d6e84fbaf22714e308db7bf308bfcd7b79b0c9fd
Lindsteez <lindgren.axel@hotmail.com>
2025-10-20 10:43:33 +0200

    added image etc of axel

---
+++ axel.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const axelBild = document.querySelector('.axel-bild');
+
+    axelBild.addEventListener('dblclick', () => {
+        alert('Axels Github https://github.com/Lindsteez');
+    });
+});
