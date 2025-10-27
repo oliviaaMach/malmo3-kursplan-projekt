60b94531cd61052448d32349a8090c7de8333d1e
Sarvin Riazi <sarvinriazi@outlook.com>
2025-10-27 08:57:38 +0100

    Added Sarvin profile image, styles, and double-click GitHub alert

---
+++ sarvin.js
@@ -0,0 +1,8 @@
+const sarvinBild = document.querySelector('#sarvin-img');
+
+
+if (sarvinBild) {
+  sarvinBild.addEventListener('dblclick', () => {
+    alert('Sarvins GitHub: https://github.com/SarvinInCode');
+  });
+}
